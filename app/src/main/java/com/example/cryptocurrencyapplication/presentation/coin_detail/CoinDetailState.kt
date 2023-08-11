f7d537f436c4b04d72dabfa7a26856fdf7bb48ac
Gülten Doğan <63645518+gultendogan0@users.noreply.github.com>
2023-08-12 01:52:05 +0300

    implementing the ViewModels

---
+++ app/src/main/java/com/example/cryptocurrencyapplication/presentation/coin_detail/CoinDetailState.kt
@@ -0,0 +1,10 @@
+package com.example.cryptocurrencyapplication.presentation.coin_detail
+
+import com.example.cryptocurrencyapplication.domain.model.Coin
+import com.example.cryptocurrencyapplication.domain.model.CoinDetail
+
+data class CoinDetailState(
+    val isLoading: Boolean = false,
+    val coin: CoinDetail? = null,
+    val error: String = ""
+)
