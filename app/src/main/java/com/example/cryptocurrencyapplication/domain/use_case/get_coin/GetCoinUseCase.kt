735034cb4fd14ef40e828b81c08eb5026dd43ce6
Gülten Doğan <63645518+gultendogan0@users.noreply.github.com>
2023-08-12 00:53:18 +0300

    implementing the use cases

---
+++ app/src/main/java/com/example/cryptocurrencyapplication/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -0,0 +1,28 @@
+package com.example.cryptocurrencyapplication.domain.use_case.get_coin
+
+import com.example.cryptocurrencyapplication.common.Resource
+import com.example.cryptocurrencyapplication.data.remote.dto.toCoinDetail
+import com.example.cryptocurrencyapplication.domain.model.Coin
+import com.example.cryptocurrencyapplication.domain.model.CoinDetail
+import com.example.cryptocurrencyapplication.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coin = repository.getCoinById(coinId).toCoinDetail()
+            emit(Resource.Success(coin))
+        } catch (e: HttpException){
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured"))
+        } catch (e: IOException){
+            emit(Resource.Error("Couldn't reach server. Check your internet connection."))
+        }
+    }
+}
