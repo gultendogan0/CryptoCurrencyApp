9f1a19f28c43fb25bdea38fccefe5b328a1783f9
Gülten Doğan <63645518+gultendogan0@users.noreply.github.com>
2023-08-12 14:26:40 +0300

    coin detail screen

--- app/src/main/java/com/example/cryptocurrencyapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencyapplication/presentation/MainActivity.kt
@@ -3,14 +3,40 @@ package com.example.cryptocurrencyapplication.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cryptocurrencyapplication.presentation.coin_detail.CoinDetailScreen
+import com.example.cryptocurrencyapplication.presentation.coin_list.CoinListScreen
 import com.example.cryptocurrencyapplication.presentation.ui.theme.CryptoCurrencyApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoCurrencyApplicationTheme {
-
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
+                }
             }
         }
     }
